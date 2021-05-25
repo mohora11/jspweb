ad038ee7bf1d6bc5c92cce24a4ff9062b33c3295
mohora11 <mohora@naver.com>
2021-05-25 12:23:32 +0900

    servlet ex

---
+++ src/ch17/textbook/NowServlet.java
@@ -0,0 +1,34 @@
+package ch17.textbook;
+
+import java.io.IOException;
+import java.util.Date;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NowServlet
+ */
+//@WebServlet("/NowServlet")
+public class NowServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head><title>현재시간</title></head>");
+		out.println("<body>");
+		out.println("현재시간은");
+		out.println(new Date());
+		out.println("입니다.");
+		out.println("</body></html>");
+		
+	}
+
+	
+}
