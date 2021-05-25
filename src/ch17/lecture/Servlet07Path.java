691bad3ff2d254f5b69ed93b1055b2a6b5b1d91a
mohora11 <mohora@naver.com>
2021-05-25 12:36:15 +0900

    servlet path

--- src/ch17/lecture/Servlet07Path.java
+++ src/ch17/lecture/Servlet07Path.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Servlet07Path
  */
-@WebServlet("/Servlet07Path/*")
+@WebServlet("/Servlet07Path/*") // * 앞에 Sevlet07Path게있음 실행됨
 public class Servlet07Path extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
