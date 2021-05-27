ef659764b32750e2470838437db60ba21f55f5ef
mohora11 <mohora@naver.com>
2021-05-27 16:48:19 +0900

    sql select

---
+++ WebContent/WEB-INF/sql/SELECT01.sql
@@ -0,0 +1,17 @@
+SELECT CustomerName,City FROM Customers;
+SELECT * FROM Customers;
+/*
+여러줄 주석
+*/
+-- 한 줄 주석 ctrl + /
+SELECT City FROM Customers; -- 8
+SELECT * FROM Categories; -- 91
+SELECT * FROM Customers; -- 10
+SELECT * FROM OrderDetails; -- 518
+SELECT * FROM Orders; -- 196
+SELECT * FROM Products; -- 77
+SELECT * FROM Shippers; -- 3
+SELECT * FROM Suppliers; -- 29
+
+SELECT * FROM Cutomers
+WHERE Country='Mexico';
