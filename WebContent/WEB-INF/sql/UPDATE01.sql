72c6b01c15101dd4afe0651cc21c7912d1f4c33a
mohora11 <mohora@naver.com>
2021-06-01 16:46:31 +0900

    sql

---
+++ WebContent/WEB-INF/sql/UPDATE01.sql
@@ -0,0 +1,29 @@
+SELECT * FROM Customers
+WHERE CustomerID = 1;
+
+UPDATE Customers
+SET CustomerName = '아이언맨'
+WHERE CustomerID = 1;
+
+UPDATE Customers
+SET CustomerName = 'thor',
+	ContactName = 'odinson',
+    Address = 'Asgard'
+WHERE CustomerID = 1;
+
+-- exercise 01
+UPDATE Customers
+SET City = 'Oslo';
+
+-- exercise 03
+UPDATE
+ Customers
+SET
+ City = 'Seoul'
+, Country
+ = 'Korea'
+ = 'Korea'
+WHERE CustomerID = 32;
+
+SELECT * FROM Employees;
+
