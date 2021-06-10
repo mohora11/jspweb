a99c4444d4a0636ace5acafa360c3a62728c74be
mohora11 <mohora@naver.com>
2021-06-10 16:12:43 +0900

    2021.06.10 sql

--- WebContent/WEB-INF/sql/GROUP04.sql
+++ WebContent/WEB-INF/sql/GROUP04.sql
@@ -24,4 +24,22 @@ ON b.memberId = m.id
 LEFT JOIN Comment c
 ON b.id = c.boardId
 GROUP BY b.id
-ORDER BY boardId DESC 
+ORDER BY boardId DESC;
+
+SELECT * FROM Member;
+
+SELECT m.*, count(b.id), count(c.id)
+FROM Member m LEFT JOIN Board b ON m.id = b.memberId
+LEFT JOIN Comment c ON m.id = c.memberId
+WHERE m.id = 'abc';
+
+SELECT * FROM Board WHERE memberId = 'abc';
+
+SELECT count(id) FROM Board
+WHERE memberId = 'abc';
+
+SELECT count(id) FROM Comment
+WHERE memberId = 'abc';
+
+select count(id) FROM Board;
+
